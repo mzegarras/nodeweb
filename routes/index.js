b0f17f976ef8375d0418d5ccfa20f817f1496d8d
manuelzegarra <mzegarra@gmail.com>
2022-01-15 05:34:12 -0500

    fast commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+const MY_VARIABLE = process.env.MY_VARIABLE
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',myVar:MY_VARIABLE});
 });
 
 module.exports = router;
